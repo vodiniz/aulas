93a92fbc3c90871a4443d386dbd14076dc003666
Vitor Oliveira Diniz <vitor.oliveira.vod@gmail.com>
2022-10-06 23:43:52 -0300

    pratica11

--- 1_periodo/introducao_programacao/pratica11/exercicio4.c
+++ 1_periodo/introducao_programacao/pratica11/exercicio4.c
@@ -44,7 +44,6 @@ int check_different_numbers(int **A, int n){
             for (int k = n - 1; k > -1; k--){
                 for (int l = n - 1 ; l > -1; l--){
                     if ( k != i || l != j){
-                        //printf("A[%d][%d] == A[%d][%d]\n", i, j, k, l);
                         if (A[i][j] == A[k][l]){
                             return 0;
                         }
@@ -62,8 +61,6 @@ int check_magic_square(int **A, int n){
     int sum = 0, sum_2 = 0;
     if(check_different_numbers(A, n)){
 
-        //printf("---------------------------------\n");
-
         // check line sum
         for ( int i = 0; i < n; i++){
             sum = 0;
@@ -73,12 +70,11 @@ int check_magic_square(int **A, int n){
             if (!i){
                 sum_2 = sum;
             }
-            //printf("%d == %d\n", sum, sum_2);
+
             if ( sum != sum_2){
                 return 0;
             }
         }
-        //printf("---------------------------------\n");
 
         // check column sum
         for ( int i = 0; i < n; i++){
@@ -87,54 +83,36 @@ int check_magic_square(int **A, int n){
                 sum += A[j][i];
             }
 
-            //printf("%d == %d\n", sum, sum_2);
-
             if ( sum != sum_2){
                 return 0;
             }
         }
-        //printf("---------------------------------\n");
 
         //sum diagonal principal
-
         sum = 0;
         for ( int i = 0; i < n; i++){
             sum += A[i][i];
         }
 
-        //printf("%d == %d\n", sum, sum_2);
-
         if ( sum != sum_2){
             return 0;
         }
         
-        //printf("---------------------------------\n");
-
         sum = 0;
-
         for ( int i = 0; i < n; i++){
-            sum += A[i][n - 1 - i];
+            sum += A[i][n - i - 1];
         }
-
-        //printf("%d == %d\n", sum, sum_2);
-            
+       
         if ( sum != sum_2){
             return 0;
         }
         
-
-        //printf("---------------------------------\n");
-
         return 1;
 
     } else {
         return 0;
     }
-
-
-
 }
-
 
 int** criaMatriz(int m, int n){
 
@@ -158,7 +136,6 @@ void print_matrix(int** A, int m, int n){
     }
 }
 
-
 void liberaMatriz(int** A, int m){
     
     for (int i = 0; i < m; i++){
@@ -166,4 +143,3 @@ void liberaMatriz(int** A, int m){
     }
     free(A);
 }
-
